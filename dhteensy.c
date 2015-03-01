ee6deded4ac27cc2f19f44ed986d7bf71e5222d8
Eric Litak <elitak@gmail.com>
2015-03-01 09:36:50 -0800

    substantial rewrite works, for the most part. still dropping a few letters during quicktype, i think, because of qsort

--- dhteensy.c
+++ dhteensy.c
@@ -2,6 +2,7 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stdlib.h>
 #include "usb_keyboard_debug.h"
 #include "print.h"
 #include "ports.h"
@@ -27,6 +28,7 @@
 #define MODE_NORMAL 0
 #define MODE_NAS 1
 #define MODE_FN 2
+#define MODE_SHIFTED 4
 
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
 
@@ -37,11 +39,11 @@ uint8_t hex_keys[16]=
 uint16_t idle_count=0;
 
 // duplicated this so that we can safely change it without worrying about interrupts
-uint8_t keys_down[16];
+#define KEYS_DOWN_MAX 16
+uint8_t keys_down[KEYS_DOWN_MAX];
 uint8_t keys_down_n;
-int lastsum;
 #define MODE_TRACK_MAX 16
-uint8_t mode_track_last[MODE_TRACK_MAX];
+uint16_t mode_track_last[MODE_TRACK_MAX];
 uint8_t mode_track_last_n=0;
 
 void init_ports(void) {
@@ -60,6 +62,69 @@ void set_led(uint8_t led) {
         port2_write(~(led>>4 & 0x0F));
 }
 
+int compare (const void * a, const void * b)
+{
+    // HACK XXX negate to avoid 'git status' bug
+    return -( *(uint8_t*)a - *(uint8_t*)b );
+}
+
+int get_modes(uint8_t *keys_down, uint8_t keys_down_n) {
+    int modes = MODE_NORMAL; // = 0
+
+    for (int i=0; i<keys_down_n; i++) { // go through each key that's currently depressed
+        int keyid = keys_down[i]; // keyid is the index into our keymapping table
+        uint16_t keycode = pgm_read_word(normal_keys + keyid); // lookup the keycode in the "normal" layer, since we're only interested in metakeys.
+
+        // XXX really either NAS or FN can be active at one time (flags interpreted later)
+        if      (keycode == KEY_DH_NAS) { modes |= MODE_NAS; }
+        else if (keycode == KEY_DH_FN) { modes |= MODE_FN; }
+        else if (keycode == KEY_DH_SHIFT) { modes |= MODE_SHIFTED; }
+
+    }
+    return modes;
+}
+
+int get_modifiers(uint8_t *keys_down, uint8_t keys_down_n) {
+    int mods = 0;
+
+    for (int i=0; i<keys_down_n; i++) {
+        int keyid = keys_down[i];
+        uint16_t keycode = pgm_read_word(normal_keys + keyid);
+
+        if (keycode == KEY_DH_SHIFT) { mods |= KEY_SHIFT; }
+        else if (keycode == KEY_DH_CTRL) { mods |= KEY_CTRL; }
+        else if (keycode == KEY_DH_ALT) { mods |= KEY_ALT; }
+        else if (keycode == 0xe3 //LeftGUI
+                 || keycode == 0xe7 //RightGUI
+                 //|| keycode == KEY_DH_NORM:
+                 ) {
+            mods |= KEY_GUI;
+        }
+    }
+    return mods;
+}
+
+// modal keys that should not count towards number depressed (for purposes of detecting change between polls)
+int get_discounted(uint8_t *keys_down, uint8_t keys_down_n) {
+    int discounted = 0;
+
+    for (int i=0; i<keys_down_n; i++) {
+        int keyid = keys_down[i];
+        uint16_t keycode = pgm_read_word(normal_keys + keyid);
+
+        switch (keycode) {
+            case KEY_DH_FN:
+            case KEY_DH_NAS:
+            case KEY_DH_CTRL: // had a bug before these were added: when using (ctrl+)alt+tab, would keep sending 'tab'
+            case KEY_DH_ALT:  // 
+            // TODO: does shift need to be here?
+                discounted++;
+        }
+    }
+    return discounted;
+}
+
+
 // TODO
 // * toggle mode in levers
 // * winkey off of lever
@@ -72,100 +137,81 @@ void set_led(uint8_t led) {
 //    - look into two reset modes (to program vs. restart code), using the external button to program would be nice.
 //    - once thats all done maybe code in different uses for the reset button based on press length and also use leds as feedback
 
-uint8_t process_keys(void) {
-
-    uint8_t k, i, nkeys=0, on_shifted_layer;
+uint8_t process_keys(uint8_t *keys_down, uint8_t keys_down_n) {
+    uint8_t keyid, i, nkeys=0, is_shifted;
     uint16_t keycode;
     uint8_t dh_keyboard_modifier_keys=0;
     uint8_t dh_keyboard_keys[6]={0,0,0,0,0,0};
     int8_t auto_shift=0;
     int8_t no_auto_shift=0;
-    uint8_t mode=MODE_NORMAL;
+    uint8_t mode;
     uint8_t kmode;
-    uint32_t sum = 0;
-    uint8_t mode_track_n=0;
-    uint8_t mode_track[MODE_TRACK_MAX];
+    uint8_t mode_track_n = 0;
+    uint16_t mode_track[MODE_TRACK_MAX];
+    static uint16_t key_track[KEYS_DOWN_MAX];
+    uint8_t something_new = 0;
+    uint8_t dont_count = 0;
 
+    // canonicalize the list of keys down by sorting it
+    qsort(keys_down, keys_down_n, sizeof(uint8_t), compare);
+    // FIXME weird bug here typing 'status' quickly omits second t, reversing this ordering fixes
+    // nana
+    // nono
 
-    // first pass for special keys
-    for (i=0; i<keys_down_n; i++) { // go through each key that's currently depressed
-        k = keys_down[i]; // k is thi index into our keymapping table
-        keycode=pgm_read_word(normal_keys+k); // lookup the keycode in our keymapping table
+    mode = get_modes(keys_down, keys_down_n);
+    dh_keyboard_modifier_keys = get_modifiers(keys_down, keys_down_n);
+    dont_count = get_discounted(keys_down, keys_down_n);
 
-        // this (along with conditional later) is an ugly hack to avoid sending
-        // new events if the only thing that has changed is the FN or NAS state
-        sum |= keycode == KEY_DH_FN || keycode == KEY_DH_NAS ? 0x80000000 : 0; // XXX represent these inconsequential key (de)presses as the highest bit, for sake of state equivalency checking after this first pass (the 'f12 bug' fix)
-        sum += keycode;
-
-        switch(keycode) {
-            case KEY_DH_NAS:
-                mode=MODE_NAS;
-                break;
-            case KEY_DH_FN:
-                mode=MODE_FN;
-                break;
-            case KEY_DH_SHIFT:
-                dh_keyboard_modifier_keys |= KEY_SHIFT;
-                break;
-            case KEY_DH_CTRL:
-                dh_keyboard_modifier_keys |= KEY_CTRL;
-                break;
-            case KEY_DH_ALT:
-                dh_keyboard_modifier_keys |= KEY_ALT;
-                break;
-            case KEY_DH_NORM:
-                dh_keyboard_modifier_keys |= KEY_GUI; // Super
-                break;
-        }
+    // Always at least set the mode-LEDs
+    if (mode & MODE_FN) {
+        set_led(LED_FN);
+    } else if (mode & MODE_NAS) {
+        set_led(LED_NAS);
+    } else {
+        set_led(LED_NORM);
     }
 
-    // set mode LEDs
-    switch(mode) {
-        case MODE_NORMAL:
-            set_led(LED_NORM);
-            break;
-        case MODE_NAS:
-            set_led(LED_NAS);
-            break;
-        case MODE_FN:
-            set_led(LED_FN);
-            break;
+    for (i=0; i<keys_down_n; i++) {
+        if (keys_down[i] != key_track[i])
+            something_new = 1;
+        key_track[i] = keys_down[i];
     }
 
-    if (sum == lastsum) // return if nothing has changed
+    if (keys_down_n - dont_count == 0) {
+        //reset key tracker in case same key is pressed next time
+        key_track[0] = 0xff;
+    } else if (!something_new) {
+        // return if nothing (besides maybe metakeys) has changed
+        // and if no keys are down at all (in that case we need to unpress the last keys)
         return 0;
-    lastsum = sum;
+    }
 
     // second pass for the rest
     for (i=0; i<keys_down_n; i++) {
-        keycode=0;
-        on_shifted_layer=0;
-        k=keys_down[i];
+        keycode = 0;
+        keyid = keys_down[i];
+        is_shifted = 0;
 
-        kmode=mode;
-        for (uint8_t j=0;j<mode_track_last_n;j++) {
-            if (k == (mode_track_last[j] & 0x3F)) { // lower 6 bits
-                kmode=(mode_track_last[j] >> 6); // if key was down previously use its original mode
+        kmode=mode; // the default mode of each key is what's currently depressed.
+        // Mode track records (modeflags)(keybyte) as short, in the order they get scanned.
+        for (uint8_t j=0; j<mode_track_last_n; j++) {
+            // Test each's lower byte for equality to see if only the mode changed.
+            if (keyid == (mode_track_last[j] & 0xff)) {
+                kmode = mode_track_last[j] >> 8; // if key was down previously use its original mode
             }
         }
+        mode_track[mode_track_n++] = (kmode << 8) + keyid; // record current mode
 
-        mode_track[mode_track_n++]=(kmode<<6)+k; // record current mode
-
-        switch(kmode) {
-            case MODE_NORMAL:
-                if (dh_keyboard_modifier_keys & KEY_SHIFT) keycode = pgm_read_word(normalS_keys+k);
-                if (!keycode) keycode = pgm_read_word(normal_keys+k); else on_shifted_layer = 1;
-                break;
-            case MODE_NAS:
-                if (dh_keyboard_modifier_keys & KEY_SHIFT) keycode = pgm_read_word(nasS_keys+k);
-                if (!keycode) keycode = pgm_read_word(nas_keys+k); else on_shifted_layer = 1;
-                break;
-            case MODE_FN:
-                if (dh_keyboard_modifier_keys & KEY_SHIFT) keycode = pgm_read_word(fnS_keys+k);
-                if (!keycode) keycode = pgm_read_word(fn_keys+k); else on_shifted_layer = 1;
-                break;
+        if (kmode & MODE_FN) {
+            if (kmode & MODE_SHIFTED) keycode = pgm_read_word(fnS_keys+keyid);
+            if (!keycode) keycode = pgm_read_word(fn_keys+keyid); else is_shifted = 1;
+        } else if (kmode & MODE_NAS) {
+            if (kmode & MODE_SHIFTED) keycode = pgm_read_word(nasS_keys+keyid);
+            if (!keycode) keycode = pgm_read_word(nas_keys+keyid); else is_shifted = 1;
+        } else {
+            if (kmode & MODE_SHIFTED) keycode = pgm_read_word(normalS_keys+keyid);
+            if (!keycode) keycode = pgm_read_word(normal_keys+keyid); else is_shifted = 1;
         }
-
 
         if (keycode & 0x80) continue; // f0-ff are special, already handled
 
@@ -173,7 +219,7 @@ uint8_t process_keys(void) {
         // keep a count of auto-shifted vs unshifted keys
         if (keycode & 1<<8)
             auto_shift++;
-        else if (on_shifted_layer)
+        else if (is_shifted)
             // only count unshifted that are pressed on shifted layers
             no_auto_shift++;
 
@@ -189,14 +235,12 @@ uint8_t process_keys(void) {
     }
     mode_track_last_n = mode_track_n;
 
-    if (no_auto_shift>0 && dh_keyboard_modifier_keys & KEY_SHIFT) {
-        // don't update if we have both auto-shift and non-auto-shift keys
+    if (no_auto_shift>0 && mode & MODE_SHIFTED) {
         if (auto_shift>0) return 0; 
-        // if we pressed a shifted key from a shifted layer, don't include the shift modifier itself.
+        // if we pressed an unshifted key from a shifted layer, don't include the shift modifier itself.
         dh_keyboard_modifier_keys ^= KEY_SHIFT;
     // we have some auto-shift keys down
     } else if (auto_shift>0) {
-        // don't update if we have both auto-shift and non-auto-shift keys
         if (no_auto_shift>0) return 0; 
         dh_keyboard_modifier_keys |= KEY_SHIFT;
     }
@@ -211,7 +255,7 @@ uint8_t process_keys(void) {
 
 uint8_t key_down(uint8_t key) {
         // XXX TODO this exhibits weird (corner case) behavior. e.g., can't
-        // repeate shifted+unshifted chars while both pressed. this is not
+        // repeat shifted+unshifted chars while both pressed. this is not
         // really an issue as the repeating is somewhat non standard to begin
         // with, like who wants 'htns' repeated in that sequence??? this
         // behavior only triggers when all keys are pressed during the same
@@ -247,11 +291,9 @@ int main(void)
 	// and do whatever it does to actually be ready for input
 	_delay_ms(1000);
 
-	/*
 	for (i=0; i<MODE_TRACK_MAX; i++) {
 		mode_track_last[i]=0;
 	}
-	*/
 	while (1) {
 		// zero out dh kbd buffer
 		for (i=0; i<16; i++) {
@@ -265,7 +307,7 @@ int main(void)
 				if(b & (1<<i))
 					key_down((selector << 2) +i);
 		}
-		process_keys();
+		process_keys(keys_down, keys_down_n);
                 if (rst_read()) {debug_dump();}
 		_delay_ms(10);
 	}
