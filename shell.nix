64e8a3ad7f70c29898e36cfe5b478194efe27128
Eric Litak <elitak@gmail.com>
2021-04-03 22:14:58 -0700

    fixed avr packages for newer nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,17 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 with pkgs;
 stdenv.mkDerivation {
   name = "dhteensy-1.0";
   src = ./.;
   buildInputs = [
-    avrbinutils # for the link script
-    avrgcc # for `avr-gcc`
-    avrlibc # for the headers needed
-    teensy-loader-cli
+    pkgsCross.avr.buildPackages.gcc # for `avr-gcc`
+    teensy-loader-cli # to program the device
   ];
-  shellHook = ''
-    export CFLAGS+=" -I${avrlibc}/avr/include"
-    export CFLAGS+=" -L${avrlibc}/avr/lib/avr51"
-    cp ${avrlibc}/avr/lib/avr51/crtat90usb1286.o . # XXX linker can't find this otherwise FIXME
-  '';
 }
